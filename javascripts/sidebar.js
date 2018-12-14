a8623216a2d62b03e3a2d7f12bb19ced18adf7e5
zetapush-ci <ci@zetapush.com>
2018-12-14 15:26:22 +0000

    Update generated documentation

--- javascripts/sidebar.js
+++ javascripts/sidebar.js
@@ -2,42 +2,43 @@
   var open = function(el) {
     $(el).addClass('opened');
     $('.sectlevel3', el).css('max-height', $('.sectlevel3', el).attr('data-height'));
-  }
+  };
   var close = function(el) {
     $(el).removeClass('opened');
     $('.sectlevel3', el).css('max-height', 0);
-  }
-  
+  };
+
   var toggle = function(el) {
-    if($(el).hasClass('opened')) {
+    if ($(el).hasClass('opened')) {
       close(el);
     } else {
       open(el);
     }
-  }
+  };
 
   var closeAll = function() {
     $('.sectlevel2 li').removeClass('opened');
     $('.sectlevel3').css('max-height', 0);
-  }
+  };
 
   var init = function() {
     $('.sectlevel3').each(function(i, el) {
       $(el).addClass('computing-size');
-      $(el).attr('data-height', $(el).outerHeight()+'px');
+      $(el).attr('data-height', $(el).outerHeight() + 'px');
       $(el).removeClass('computing-size');
     });
     $('.sectlevel2 li').each(function(i, el) {
-      if($('.sectlevel3', el).length) {
+      if ($('.sectlevel3', el).length) {
         $(el).addClass('has-subsection');
         $('> a', el).append('<i class="fa fa-caret-right"></i>');
       }
     });
     $('.sectlevel2 li').click(function(e) {
+      e.stopPropagation();
       toggle($(e.currentTarget));
     });
     closeAll();
-  }
+  };
 
-  $(document).ready(init);  
+  $(document).ready(init);
 })();
--- javascripts/sourcecode/source-utils.js
+++ javascripts/sourcecode/source-utils.js
@@ -1,61 +1,75 @@
 (function() {
-	var convert = function(/*String[]*/sourceLines, /*String*/lines) {
-		var hl = [];
-		var parts = lines.split(",");
-		// example: 1,5,10-25
-		for(var i=0 ; i<parts.length ; i++) {
-			var part = parts[i];
-			if(part.indexOf("-")!=-1) {
-				// example: 10-25
-				var subpart = part.split("-");
-				var start = subpart[0] ? parseInt(subpart[0]) : 1;
-				var end = subpart[1] ? parseInt(subpart[1]) : sourceLines.length;
-				for(var j=start ; j<=end ; j++) {
-					hl.push(j);
-				}
-			} else {
-				// simple number
-				hl.push(parseInt(part));
-			}
-		}
-		return hl;
-	};
-	
-	var update = function(/*Node*/element, /*Array*/lines) {
-		element.html(lines.join("\n"));
-	};
-	
-	var getSourceLines = function(/*Node*/element) {
-		var content = element.html();
-		return content.split("\n");
-	};
-	
-	var getSourceElement = function(/*Node*/listingBlock) {
-		return $(listingBlock).find("pre code");
-	}
-	
-	var parseLinesMetadata = function(/*String*/metadata) {
-		var classes = metadata.split(' ');
-		var classMap = {};
-		for(var i=0 ; i<classes.length ; i++) {
-			var cssClass = classes[i];
-			var parts = cssClass.split(':');
-			if(parts.length==2) {
-				classMap[parts[0]] = parts[1];
-			}
-		}
-		return classMap;
-	}
-	
-	window.lineUtils = {
-		convert: convert,
-		update: update,
-		getSourceLines: getSourceLines		
-	}
-	
-	window.sourceUtils = {
-		getSourceElement: getSourceElement,
-		parseLinesMetadata: parseLinesMetadata
-	}
-})();
+  var convert = function(/*{lineNumber: String[], code: String[]}*/ sourceLines, /*String*/ lines) {
+    var hl = [];
+    var parts = lines.split(',');
+    // example: 1,5,10-25
+    for (var i = 0; i < parts.length; i++) {
+      var part = parts[i];
+      if (part.indexOf('-') != -1) {
+        // example: 10-25
+        var subpart = part.split('-');
+        var start = subpart[0] ? parseInt(subpart[0]) : 1;
+        var end = subpart[1] ? parseInt(subpart[1]) : sourceLines.code.length;
+        for (var j = start; j <= end; j++) {
+          hl.push(j);
+        }
+      } else {
+        // simple number
+        hl.push(parseInt(part));
+      }
+    }
+    return hl;
+  };
 
+  var update = function(/*Node*/ element, /*{lineNumber: String[], code: String[]}*/ lines) {
+    if (lines.lineNumbers.length) {
+      element.find('table.CodeRay .line-numbers pre').html(lines.lineNumbers.join('\n'));
+      element.find('table.CodeRay .code pre').html(lines.code.join('\n'));
+    } else {
+      element.html(lines.code.join('\n'));
+    }
+  };
+
+  var getSourceLines = function(/*Node*/ element) /*: {lineNumber: String[], code: String[]}*/ {
+    // there are line numbers so code section is different
+    var lineNumbers = [];
+    var codeElement = element;
+    if (element.find('table.CodeRay .line-numbers').length) {
+      lineNumbers = element
+        .find('table.CodeRay .line-numbers pre')
+        .html()
+        .split('\n');
+      codeElement = element.find('table.CodeRay .code pre');
+    }
+    var content = codeElement.html();
+    return { lineNumbers, code: content.split('\n') };
+  };
+
+  var getSourceElement = function(/*Node*/ listingBlock) {
+    return $(listingBlock).find('pre code');
+  };
+
+  var parseLinesMetadata = function(/*String*/ metadata) {
+    var classes = metadata.split(' ');
+    var classMap = {};
+    for (var i = 0; i < classes.length; i++) {
+      var cssClass = classes[i];
+      var parts = cssClass.split(':');
+      if (parts.length == 2) {
+        classMap[parts[0]] = parts[1];
+      }
+    }
+    return classMap;
+  };
+
+  window.lineUtils = {
+    convert: convert,
+    update: update,
+    getSourceLines: getSourceLines
+  };
+
+  window.sourceUtils = {
+    getSourceElement: getSourceElement,
+    parseLinesMetadata: parseLinesMetadata
+  };
+})();
